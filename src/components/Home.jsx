14fd8370c70a4265f467627427d94b2cb3cb443e
tommdq <registom123@gmail.com>
2022-10-10 18:45:46 -0300

    eslint - prettier config

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
-
 const Home = () => {
-  return (
-      <header className="text-center py-10">
-        <h1>Zeta Trip</h1>
-        <br />
-        <h2>Vitácoras de viajes en Argentina</h2>
-      </header>
-  )
+	return (
+		<header className='py-10 text-center'>
+			<h1>Zeta Trip</h1>
+			<br />
+			<h2>Vitácoras de viajes en Argentina</h2>
+		</header>
+	)
 }
 
 export default Home
